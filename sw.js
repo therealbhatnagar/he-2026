70eed483303695876b80e9d3525876c937463b01
therealbhatnagar <therealbhatnagar@gmail.com>
2026-04-06 16:55:11 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'highenough-v1';
+const ASSETS_TO_CACHE = ['/'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      const fetchPromise = fetch(event.request).then((networkResponse) => {
+        if (networkResponse.ok) {
+          const cacheCopy = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, cacheCopy);
+          });
+        }
+        return networkResponse;
+      }).catch(() => cachedResponse);
+      return cachedResponse || fetchPromise;
+    })
+  );
+});
